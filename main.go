97ba009401b923982c33436a5994f6a711b1a642
vaibhavvvvv <vaibhavng7@gmail.com>
2024-10-29 01:41:56 +0530

    Remove Sudo

--- main.go
+++ main.go
@@ -1,20 +1,12 @@
 package main
 
 import (
+	"erebrusvps/docker"
 	"log"
-	"os"
-
-	docker "erebrusvps/docker"
 )
 
 func main() {
-	// Get sudo password from environment variable for security
-	sudoPassword := os.Getenv("SUDO_PASSWORD")
-	if sudoPassword == "" {
-		log.Fatal("Please set SUDO_PASSWORD environment variable")
-	}
-
-	dockerSetup := docker.NewDockerSetup(sudoPassword)
+	dockerSetup := docker.NewDockerSetup()
 	if err := dockerSetup.Install(); err != nil {
 		log.Fatalf("Docker setup failed: %v", err)
 	}
